83df1d1857bc643e2e87f4c2d19e2eef99a58d41
Aakash <anushabhatnagar@Anushas-MacBook-Air.local>
2019-02-18 00:43:42 +0530

    Form related changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-var dataCacheName = 'weatherData-v1';
 var cacheName = 'indelible events';
 var filesToCache = [
   '/',
@@ -19,33 +18,9 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://www.indelible.events/';
-  if (e.request.url.indexOf(dataUrl) === -1) {
-    /*
-     * When the request URL contains dataUrl, the app is asking for fresh
-     * weather data. In this case, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
-    e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
-      })
-    );
-  } else {
-    /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
     e.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
       })
     );
-  }
 });
